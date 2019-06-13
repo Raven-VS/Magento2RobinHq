52889ddf21417f2d92ec83abc3ba823dad43b188
Bram Gerritsen <bgerritsen@emico.nl>
2019-06-13 16:27:45 +0200

    WIP first API endpoint

--- src/Controller/Api/Customer.php
+++ src/Controller/Api/Customer.php
@@ -1,18 +1,25 @@
 <?php
 namespace Emico\RobinHq\Controller\Api;
 
+use Emico\RobinHqLib\Server\RestApiServer;
 use Magento\Framework\App\Action\Action;
 
 class Customer extends Action
 {
     protected $_pageFactory;
+    /**
+     * @var RestApiServer
+     */
+    private $restApiServer;
 
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory)
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        RestApiServer $restApiServer)
     {
         $this->_pageFactory = $pageFactory;
         parent::__construct($context);
+        $this->restApiServer = $restApiServer;
     }
 
     public function execute()
---
+++ src/Model/Config.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Emico\RobinHq\Model;
+
+use Emico\RobinHqLib\Config\ConfigInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * @author Bram Gerritsen <bgerritsen@emico.nl>
+ * @copyright (c) Emico B.V. 2017
+ */
+
+class Config implements ConfigInterface
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $config;
+
+    /**
+     * Config constructor.
+     * @param ScopeConfigInterface $config
+     */
+    public function __construct(ScopeConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiKey(): string
+    {
+        return $this->config->getValue('robinhq/api/key', ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiSecret(): string
+    {
+        return $this->config->getValue('robinhq/api/secret', ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiUri(): string
+    {
+        return $this->config->getValue('robinhq/api/url', ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiServerKey(): string
+    {
+        return $this->config->getValue('robinhq/api/server_key', ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiServerSecret(): string
+    {
+        return $this->config->getValue('robinhq/api/server_secret', ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isApiEnabled(): bool
+    {
+        return $this->config->getValue('robinhq/api/server_enabled', ScopeInterface::SCOPE_STORE);
+    }
+}
