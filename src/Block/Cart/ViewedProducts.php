6a26caaa9432453fa853a920fdf5cb515396abe8
Bram Gerritsen <bgerritsen@emico.nl>
2019-06-28 16:06:26 +0200

    WIP viewed products

---
+++ src/Block/Cart/ViewedProducts.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Emico\RobinHq\Block\Cart;
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * @author Bram Gerritsen <bgerritsen@emico.nl>
+ * @copyright (c) Emico B.V. 2017
+ */
+
+class ViewedProducts extends Template
+{
+    public function getTest(): string
+    {
+        return 'foo';
+    }
+}
